a1ac826d0830dc638d74dba09e0e3b3bcf49a14c
Vadim <vdmkonshin@gmail.com>
2024-05-02 13:45:52 +0300

    add connector api

---
+++ src/connected.py
@@ -0,0 +1,12 @@
+import requests
+
+
+class ApiHH:
+    def __init__(self, employer_id):
+        self.url = 'https://api.hh.ru/employers'
+        self.employer_id = employer_id
+
+    def get_vacancy_api(self):
+        response = requests.get(f'https://api.hh.ru/vacancies?employer_id={self.employer_id}&per_page=10')
+        data_vacancy = response.json()['items']
+        return data_vacancy
