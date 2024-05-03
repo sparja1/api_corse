f7607683969f7e6ea78c1924e68b88ceed121d60
Vadim <vdmkonshin@gmail.com>
2024-05-04 00:38:50 +0300

    refactor code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,35 @@
 from src.api import ApiHH
 from src.db_manager import DBManager
-from src.hh import VacancyHH
+
 from src.utils import save_to_json
+
+
+class VacancyHH:
+    def __init__(self, name_vacancy: str, salary: int, short_description: str,
+                 requirements: str, apply_alternate_url: str, id_vacancy: int, id_company: int):
+        self.name_vacancy = name_vacancy
+        if salary:
+            self.salary = salary['from']
+        elif salary:
+            self.salary = salary['to']
+        else:
+            self.salary = 0
+        self.short_description = short_description if short_description else 'Краткое описание не указано'
+        self.requirements = requirements if requirements else 'Требования не указаны'
+        self.apply_alternate_url = apply_alternate_url
+        self.id_vacancy = id_vacancy
+        self.id_company = id_company
+
+    def to_dict(self):
+        data_vacancy = {
+            'id_company': self.id_company,
+            'id_vacancy': self.id_vacancy,
+            'name_vacancy': self.name_vacancy,
+            'salary': self.salary,
+            'short_description': self.short_description,
+            'requirements': self.requirements,
+            'apply_alternate_url': self.apply_alternate_url}
+        return data_vacancy
 
 
 def main():
@@ -39,7 +67,7 @@ def main():
         print("2. Получить список всех вакансий")
         print("3. Получить среднюю зарплату по вакансиям")
         print("4. Получить список всех вакансий, у которых зарплата выше средней по всем вакансиям")
-        print("5. Получить список всех вакансий, в названии которых содержатся переданные слова")
+        print("5. Получить список всех вакансий, в названии которых содержатся переданные в метод слова")
         print("6. Выход")
         choice = input("Введите номер вашего выбора: ")
         if choice == '1':
